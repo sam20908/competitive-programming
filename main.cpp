66bcebb68f2cf08b814a71ff35cf62be3a1ffebc
Sam Huang <samestimable2016@gmail.com>
2022-08-14 09:34:20 -0700

    Add union-find template

--- main.cpp
+++ main.cpp
@@ -75,6 +75,31 @@ constexpr int gcd(int a, int b) {
 }
 constexpr int lcm(int a, int b) { return (a * b) / gcd(a, b); }
 
+struct dsu {
+  vec<int> parent, rank;
+  int distinct;
+  dsu(int n) : parent(n), rank(n, 1), distinct(n) {
+    for (int i = 0; i < n; i++)
+      parent[i] = i;
+  }
+  int find(int i) {
+    if (parent[i] == i)
+      return i;
+    return parent[i] = find(parent[i]);
+  }
+  void unite(int i, int j) {
+    if (find(i) != find(j)) {
+      distinct--;
+      int repi = find(i);
+      int repj = find(j);
+      if (rank[repi] < rank[repj])
+        swap(repi, repj);
+      parent[repj] = repi;
+      rank[repi] += int(rank[repi] == rank[repj]);
+    }
+  }
+};
+
 // ----- CHANGE FOR PROBLEM -----
 class Solution {
 public:
