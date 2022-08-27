e76d19de22bef8d84e3660183102234f21fc3680
Sam Huang <samestimable2016@gmail.com>
2022-08-27 12:21:06 -0700

    Allow around 250MB of stack space

--- run.bat
+++ run.bat
@@ -5,21 +5,21 @@ for /f "tokens=* usebackq" %%f in (`where cl`) do set clvar=%%f
 if "%clvar%"=="%clvar:nocompiler=%" (
   echo Found MSVC as the compiler
   echo.
-  cl /DLC_LOCAL /Zc:preprocessor /Z7 /std:c++17 /EHsc /nologo main.cpp && goto exec
+  cl /DLC_LOCAL /F 268435456 /Zc:preprocessor /Z7 /std:c++17 /EHsc /nologo main.cpp && goto exec
   goto compileerr
 )
 for /f "tokens=* usebackq" %%f in (`where g++`) do set gccvar=%%f
 if "%gccvar%"=="%gccvar:nocompiler=%" (
   echo Found GCC as the compiler
   echo.
-  g++ -DLC_LOCAL -std=c++17 -Wshadow main.cpp -g -o main.exe && goto exec
+  g++ -DLC_LOCAL -std=c++17 -Wshadow main.cpp -g -o main.exe -Wl,--stack,268435456 && goto exec
   goto compileerr
 )
 for /f "tokens=* usebackq" %%f in (`where clang++`) do set clangvar=%%f
 if "%clangvar%"=="%clangvar:nocompiler=%" (
   echo Found Clang as the compiler
   echo.
-  clang++ -DLC_LOCAL -std=c++17 -Wshadow main.cpp -g -o main.exe && goto exec
+  clang++ -DLC_LOCAL -std=c++17 -Wshadow main.cpp -g -o main.exe -Wl,--stack,268435456 && goto exec
   goto compileerr
 )
 
