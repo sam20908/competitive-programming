ce3479dae4c7b8980cf35a4be7a25efb8d46f339
Sam Huang <samestimable2016@gmail.com>
2022-09-16 12:15:13 -0700

    Add binary exponentiation with modulo

--- templates/math_common.cpp
+++ templates/math_common.cpp
@@ -4,3 +4,15 @@ constexpr int gcd(int a, int b) {
   return gcd(b, a % b);
 }
 constexpr int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+constexpr ll binpow(ll a, ll b, ll m) {
+  a %= m;
+  ll res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
