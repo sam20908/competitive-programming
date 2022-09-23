24de8a4a8895ea73c228533b8879ceacdeb96a80
Sam Huang <samestimable2016@gmail.com>
2022-09-23 08:52:17 -0700

    Add mod_int

---
+++ templates/mod_int.cpp
@@ -0,0 +1,14 @@
+struct mint {
+  ll v{};
+  mint(ll vx) : v(vx % M) {}
+  friend mint operator+(const mint &a, const mint &b) { return {(a.v + b.v) % M}; }
+  friend mint operator+(ll a, const mint &b) { return {(a + b.v) % M}; }
+  friend mint operator+(const mint &a, ll b) { return {(a.v + b) % M}; }
+  friend mint operator*(const mint &a, const mint &b) { return {(a.v * b.v) % M}; }
+  friend mint operator*(ll a, const mint &b) { return {(a * b.v) % M}; }
+  friend mint operator*(const mint &a, ll b) { return {(a.v * b) % M}; }
+  mint &operator+=(const mint &a) {
+    v = (v + a.v) % M;
+    return *this;
+  }
+};
