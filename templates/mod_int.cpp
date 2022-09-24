78f75f1482b17b5939007ec133d1238cf353eded
Sam Huang <samestimable2016@gmail.com>
2022-09-24 06:29:06 -0700

    Make sure to adjust negative values

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -1,6 +1,6 @@
 struct mint {
   ll v{};
-  mint(ll vx) : v(vx % M) {}
+  mint(ll vx) : v((vx % M + M) % M) {}
   friend mint operator+(const mint &a, const mint &b) { return {(a.v + b.v) % M}; }
   friend mint operator+(ll a, const mint &b) { return {(a + b.v) % M}; }
   friend mint operator+(const mint &a, ll b) { return {(a.v + b) % M}; }
