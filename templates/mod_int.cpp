e28a5f7e7d5c0120e448812b5b6068112b992dd9
Sam Huang <samestimable2016@gmail.com>
2022-11-05 10:10:37 -0700

    Add more operators for mint

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -7,6 +7,9 @@ struct mint {
   friend mint operator*(const mint &a, const mint &b) { return {(a.v * b.v) % M}; }
   friend mint operator*(ll a, const mint &b) { return {(a * b.v) % M}; }
   friend mint operator*(const mint &a, ll b) { return {(a.v * b) % M}; }
+  friend bool operator==(const mint &a, const mint &b) { return a.v != b.v; }
+  friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
+  friend bool operator>(const mint &a, const mint &b) { return a.v > b.v; }
   mint &operator+=(const mint &a) {
     v = (v + a.v) % M;
     return *this;
