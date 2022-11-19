6ac4ec478ce1416eac5d5c32cfcd8a5cb4daf789
Sam Huang <samestimable2016@gmail.com>
2022-11-19 14:35:34 -0800

    Cleanup (and fix) mod_int

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -5,14 +5,12 @@ struct mint {
   friend mint operator+(const mint &a, const mint &b) { return {(a.v + b.v) % M}; }
   friend mint operator+(long long a, const mint &b) { return {(a + b.v) % M}; }
   friend mint operator+(const mint &a, long long b) { return {(a.v + b) % M}; }
+  friend mint operator-(const mint &a, const mint &b) { return {(a.v - b.v + M) % M}; }
+  friend mint operator-(long long a, const mint &b) { return {(a - b.v + M) % M}; }
+  friend mint operator-(const mint &a, long long b) { return {(a.v - b + M) % M}; }
   friend mint operator*(const mint &a, const mint &b) { return {(a.v * b.v) % M}; }
   friend mint operator*(long long a, const mint &b) { return {(a * b.v) % M}; }
   friend mint operator*(const mint &a, long long b) { return {(a.v * b) % M}; }
-  friend bool operator==(const mint &a, const mint &b) { return a.v != b.v; }
-  friend bool operator<(const mint &a, const mint &b) { return a.v < b.v; }
-  friend bool operator>(const mint &a, const mint &b) { return a.v > b.v; }
-  mint &operator+=(const mint &a) {
-    v = (v + a.v) % M;
-    return *this;
-  }
+  friend bool operator==(const mint &a, const mint &b) { return a.v == b.v; }
+  friend bool operator!=(const mint &a, const mint &b) { return a.v != b.v; }
 };
