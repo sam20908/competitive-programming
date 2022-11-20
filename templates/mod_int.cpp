6443730dfc1eb0777ef94747bc006ff909bb1513
Sam Huang <samestimable2016@gmail.com>
2022-11-19 22:06:22 -0800

    Rename to mod_int

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -1,16 +1,16 @@
-struct mint {
+struct mod_int {
   long long v{};
-  mint() = default;
-  mint(long long vx) : v((vx % M + M) % M) {}
-  friend mint operator+(const mint &a, const mint &b) { return {(a.v + b.v) % M}; }
-  friend mint operator+(long long a, const mint &b) { return {(a + b.v) % M}; }
-  friend mint operator+(const mint &a, long long b) { return {(a.v + b) % M}; }
-  friend mint operator-(const mint &a, const mint &b) { return {(a.v - b.v + M) % M}; }
-  friend mint operator-(long long a, const mint &b) { return {(a - b.v + M) % M}; }
-  friend mint operator-(const mint &a, long long b) { return {(a.v - b + M) % M}; }
-  friend mint operator*(const mint &a, const mint &b) { return {(a.v * b.v) % M}; }
-  friend mint operator*(long long a, const mint &b) { return {(a * b.v) % M}; }
-  friend mint operator*(const mint &a, long long b) { return {(a.v * b) % M}; }
-  friend bool operator==(const mint &a, const mint &b) { return a.v == b.v; }
-  friend bool operator!=(const mint &a, const mint &b) { return a.v != b.v; }
+  mod_int() = default;
+  mod_int(long long vx) : v((vx % M + M) % M) {}
+  friend mod_int operator+(const mod_int &a, const mod_int &b) { return {(a.v + b.v) % M}; }
+  friend mod_int operator+(long long a, const mod_int &b) { return {(a + b.v) % M}; }
+  friend mod_int operator+(const mod_int &a, long long b) { return {(a.v + b) % M}; }
+  friend mod_int operator-(const mod_int &a, const mod_int &b) { return {(a.v - b.v + M) % M}; }
+  friend mod_int operator-(long long a, const mod_int &b) { return {(a - b.v + M) % M}; }
+  friend mod_int operator-(const mod_int &a, long long b) { return {(a.v - b + M) % M}; }
+  friend mod_int operator*(const mod_int &a, const mod_int &b) { return {(a.v * b.v) % M}; }
+  friend mod_int operator*(long long a, const mod_int &b) { return {(a * b.v) % M}; }
+  friend mod_int operator*(const mod_int &a, long long b) { return {(a.v * b) % M}; }
+  friend bool operator==(const mod_int &a, const mod_int &b) { return a.v == b.v; }
+  friend bool operator!=(const mod_int &a, const mod_int &b) { return a.v != b.v; }
 };
