c8c86a30a8d025a82ab08da8ee90f4357edafbbb
Sam Huang <samestimable2016@gmail.com>
2022-11-24 13:12:26 -0800

    Fix compile error with mod_int

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -3,7 +3,10 @@ struct mod_int {
   long long v{};
   mod_int(mod_int_negative) : v{-1} {}
   mod_int(long long vx) : v((vx + M) % M) {}
-  mod_int &operator=(long long vx) { v = (vx + M) % M; }
+  mod_int &operator=(long long vx) {
+    v = (vx + M) % M;
+    return *this;
+  }
   friend mod_int operator+(const mod_int &a, const mod_int &b) { return {(a.v + b.v) % M}; }
   friend mod_int operator+(long long a, const mod_int &b) { return {((a + M) % M + b.v) % M}; }
   friend mod_int operator+(const mod_int &a, long long b) { return {(a.v + (b + M) % M) % M}; }
