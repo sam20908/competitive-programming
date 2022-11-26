769e2244fd681a0e1fdd8fea4f6856537c345252
Sam Huang <samestimable2016@gmail.com>
2022-11-26 11:06:40 -0800

    More fixes

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -1,19 +1,20 @@
 template <int MOD = 1'000'000'007> struct mod_int {
   long long v{};
-  mod_int(long long vx) : v((vx + M) % M) {}
+  mod_int() = default;
+  mod_int(long long vx) : v((vx + MOD) % MOD) {}
   mod_int &operator=(long long vx) {
-    v = (vx + M) % M;
+    v = (vx + MOD) % MOD;
     return *this;
   }
   friend mod_int operator+(const mod_int &a, const mod_int &b) { return (a.v + b.v) % MOD; }
   friend mod_int operator+(long long a, const mod_int &b) { return ((a + MOD) % MOD + b.v) % MOD; }
-  friend mod_int operator+(const mod_int &a, long long b) { return (a.v + (b + MOD) % M) % MOD; }
+  friend mod_int operator+(const mod_int &a, long long b) { return (a.v + (b + MOD) % MOD) % MOD; }
   friend mod_int operator-(const mod_int &a, const mod_int &b) { return (a.v - b.v + MOD) % MOD; }
   friend mod_int operator-(long long a, const mod_int &b) { return ((a + MOD) % MOD - b.v + MOD) % MOD; }
   friend mod_int operator-(const mod_int &a, long long b) { return (a.v - (b + MOD) % MOD + MOD) % MOD; }
   friend mod_int operator*(const mod_int &a, const mod_int &b) { return (a.v * b.v) % MOD; }
-  friend mod_int operator*(long long a, const mod_int &b) { return (((a + MOD) % M) * b.v) % MOD; }
-  friend mod_int operator*(const mod_int &a, long long b) { return (a.v * ((b + MOD) % M)) % MOD; }
+  friend mod_int operator*(long long a, const mod_int &b) { return (((a + MOD) % MOD) * b.v) % MOD; }
+  friend mod_int operator*(const mod_int &a, long long b) { return (a.v * ((b + MOD) % MOD)) % MOD; }
   friend bool operator==(const mod_int &a, const mod_int &b) { return a.v == b.v; }
   friend bool operator!=(const mod_int &a, const mod_int &b) { return a.v != b.v; }
 };
