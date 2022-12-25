205a289986d46ba167f09204db051f2c6b616224
Sam Huang <samestimable2016@gmail.com>
2022-12-24 19:16:41 -0800

    For order of operation bug for mod_int

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -8,14 +8,14 @@ template <int MOD = 1'000'000'007> struct mod_int {
   }
 #define OP(op)                                                                                                         \
   friend mod_int operator op(const mod_int &a, const mod_int &b) { return (a.v op b.v) % MOD; }                        \
-  friend mod_int operator op(long long a, const mod_int &b) { return ((a + MOD) % MOD op b.v) % MOD; }                 \
-  friend mod_int operator op(const mod_int &a, long long b) { return (a.v op(b + MOD) % MOD) % MOD; }                  \
+  friend mod_int operator op(long long a, const mod_int &b) { return (((a + MOD) % MOD) op b.v) % MOD; }                 \
+  friend mod_int operator op(const mod_int &a, long long b) { return (a.v op((b + MOD) % MOD)) % MOD; }                  \
   mod_int &operator op##=(const mod_int &other) {                                                                      \
     v = (v op other.v) % MOD;                                                                                          \
     return *this;                                                                                                      \
   }                                                                                                                    \
   mod_int &operator op##=(long long other) {                                                                           \
-    v = (v op(other + MOD) % MOD) % MOD;                                                                               \
+    v = (v op((other + MOD) % MOD)) % MOD;                                                                               \
     return *this;                                                                                                      \
   }
   OP(+)
