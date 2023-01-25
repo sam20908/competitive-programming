e82d8103244ced1309576d9a693074de10e9f783
Sam Huang <samestimable2016@gmail.com>
2023-01-25 09:46:09 -0800

    Template for "graph cut" for both cut edge and vertex

---
+++ templates/graph_cut.cpp
@@ -0,0 +1,26 @@
+struct graph_cut {
+  vector<int> disc, low, is_cut_vertex, is_cut_edge;
+  graph_cut(vector<vector<pair<int, int>>> &adj, int edges)
+      : disc(adj.size()), low(adj.size()), is_cut_vertex(adj.size()), is_cut_edge(edges) {
+    int time = 0;
+    auto f = [&](auto &self, int cur, int prev) -> int {
+      int childrens = 0;
+      disc[cur] = low[cur] = ++time;
+      for (auto [next, edge_id] : adj[cur]) {
+        if (next == prev)
+          continue;
+        if (!disc[next]) {
+          childrens++;
+          self(self, next, cur);
+          is_cut_vertex[cur] = disc[cur] <= low[next];
+          is_cut_edge[edge_id] = disc[cur] < low[next];
+          low[cur] = min(low[cur], low[next]);
+        } else
+          low[cur] = min(low[cur], disc[next]);
+      }
+      return childrens;
+    };
+    for (int i = 0; i < adj.size(); i++)
+      is_cut_vertex[i] |= !disc[i] && f(f, i, i) > 1;
+  }
+};
