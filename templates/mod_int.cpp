b0c27734bc157d569fcd3fb22226bb90f791899f
Sam Huang <samestimable2016@gmail.com>
2023-02-11 13:02:23 -0800

    Fix mod_int operator ambiguity

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -1,21 +1,16 @@
-template <int MOD = 1'000'000'007> struct mod_int {
-  long long v{};
+template <int MOD = 1'000'000'007, typename T = long long> struct mod_int {
+  T v{};
   mod_int() = default;
-  mod_int(long long vx) : v((vx + MOD) % MOD) {}
-  mod_int &operator=(long long vx) {
+  mod_int(T vx) : v((vx + MOD) % MOD) {}
+  mod_int &operator=(T vx) {
     v = (vx + MOD) % MOD;
     return *this;
   }
 #define OP(op)                                                                                                         \
   friend mod_int operator op(const mod_int &a, const mod_int &b) { return (a.v op b.v) % MOD; }                        \
-  friend mod_int operator op(long long a, const mod_int &b) { return (((a + MOD) % MOD) op b.v) % MOD; }               \
-  friend mod_int operator op(const mod_int &a, long long b) { return (a.v op((b + MOD) % MOD)) % MOD; }                \
+  friend mod_int operator op(T a, const mod_int &b) { return (((a + MOD) % MOD) op b.v) % MOD; }                       \
   mod_int &operator op##=(const mod_int &other) {                                                                      \
     v = (v op other.v) % MOD;                                                                                          \
-    return *this;                                                                                                      \
-  }                                                                                                                    \
-  mod_int &operator op##=(long long other) {                                                                           \
-    v = (v op((other + MOD) % MOD)) % MOD;                                                                             \
     return *this;                                                                                                      \
   }
   OP(+)
@@ -24,6 +19,5 @@ template <int MOD = 1'000'000'007> struct mod_int {
 #undef OP
   friend bool operator==(const mod_int &a, const mod_int &b) { return a.v == b.v; }
   friend bool operator!=(const mod_int &a, const mod_int &b) { return a.v != b.v; }
-  using conv_t = long long;
-  operator conv_t() { return v; }
+  explicit operator T() { return v; }
 };
