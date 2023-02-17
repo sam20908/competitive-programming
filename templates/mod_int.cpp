177e69b92aaff21723c6edfad49d125f911532ea
Sam Huang <samestimable2016@gmail.com>
2023-02-17 09:53:00 -0800

    Make mod_int constexpr compatible

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -1,15 +1,19 @@
 template <int MOD = 1'000'000'007, typename T = long long> struct mod_int {
   T v{};
-  mod_int() = default;
-  mod_int(T vx) : v((vx + MOD) % MOD) {}
-  mod_int &operator=(T vx) {
+  constexpr mod_int() = default;
+  constexpr mod_int(T vx) : v((vx + MOD) % MOD) {}
+  constexpr mod_int &operator=(T vx) {
     v = (vx + MOD) % MOD;
     return *this;
   }
 #define OP(op)                                                                                                         \
-  friend mod_int operator op(const mod_int &a, const mod_int &b) { return ((a.v op b.v) % MOD + MOD) % MOD; }          \
-  friend mod_int operator op(T a, const mod_int &b) { return (((a % MOD + MOD) % MOD op b.v) % MOD + MOD) % MOD; }     \
-  mod_int &operator op##=(const mod_int &other) {                                                                      \
+  constexpr friend mod_int operator op(const mod_int &a, const mod_int &b) {                                           \
+    return ((a.v op b.v) % MOD + MOD) % MOD;                                                                           \
+  }                                                                                                                    \
+  constexpr friend mod_int operator op(T a, const mod_int &b) {                                                        \
+    return (((a % MOD + MOD) % MOD op b.v) % MOD + MOD) % MOD;                                                         \
+  }                                                                                                                    \
+  constexpr mod_int &operator op##=(const mod_int &other) {                                                            \
     v = ((v op other.v) % MOD + MOD) % MOD;                                                                            \
     return *this;                                                                                                      \
   }
@@ -17,6 +21,6 @@ template <int MOD = 1'000'000'007, typename T = long long> struct mod_int {
   OP(-)
   OP(*)
 #undef OP
-  friend bool operator==(const mod_int &a, const mod_int &b) { return a.v == b.v; }
-  friend bool operator!=(const mod_int &a, const mod_int &b) { return a.v != b.v; }
+  constexpr friend bool operator==(const mod_int &a, const mod_int &b) { return a.v == b.v; }
+  constexpr friend bool operator!=(const mod_int &a, const mod_int &b) { return a.v != b.v; }
 };
