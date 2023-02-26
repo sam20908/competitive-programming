760469872aa5f6d8d44284feee66b47a0c82c71d
Sam Huang <samestimable2016@gmail.com>
2023-02-26 13:21:41 -0800

    Fix suffix array

--- templates/suffix_array.cpp
+++ templates/suffix_array.cpp
@@ -4,11 +4,11 @@ vector<int> sa(string &s, int alphabet = 256) {
   int n = s.size();
   vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
   for (int i = 0; i < n; i++)
-    cnt[s[i] - begin]++;
+    cnt[s[i]]++;
   for (int i = 1; i < alphabet; i++)
     cnt[i] += cnt[i - 1];
   for (int i = 0; i < n; i++)
-    p[--cnt[s[i] - begin]] = i;
+    p[--cnt[s[i]]] = i;
   c[p[0]] = 0;
   int classes = 1;
   for (int i = 1; i < n; i++) {
