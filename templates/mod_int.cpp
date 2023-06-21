598feae8013b34be879f7a3dc713f2418daa4918
Sam Huang <samestimable2016@gmail.com>
2023-06-21 10:25:30 -0700

    Re-formatted mod_int

--- templates/mod_int.cpp
+++ templates/mod_int.cpp
@@ -6,16 +6,16 @@ template <int MOD = 1'000'000'007, typename T = long long> struct mod_int {
     v = (vx + MOD) % MOD;
     return *this;
   }
-#define OP(op)                                                                                                         \
-  constexpr friend mod_int operator op(const mod_int &a, const mod_int &b) {                                           \
-    return ((a.v op b.v) % MOD + MOD) % MOD;                                                                           \
-  }                                                                                                                    \
-  constexpr friend mod_int operator op(T a, const mod_int &b) {                                                        \
-    return (((a % MOD + MOD) % MOD op b.v) % MOD + MOD) % MOD;                                                         \
-  }                                                                                                                    \
-  constexpr mod_int &operator op##=(const mod_int &other) {                                                            \
-    v = ((v op other.v) % MOD + MOD) % MOD;                                                                            \
-    return *this;                                                                                                      \
+#define OP(op)                                                               \
+  constexpr friend mod_int operator op(const mod_int &a, const mod_int &b) { \
+    return ((a.v op b.v) % MOD + MOD) % MOD;                                 \
+  }                                                                          \
+  constexpr friend mod_int operator op(T a, const mod_int &b) {              \
+    return (((a % MOD + MOD) % MOD op b.v) % MOD + MOD) % MOD;               \
+  }                                                                          \
+  constexpr mod_int &operator op##=(const mod_int &other) {                  \
+    v = ((v op other.v) % MOD + MOD) % MOD;                                  \
+    return *this;                                                            \
   }
   OP(+)
   OP(-)
