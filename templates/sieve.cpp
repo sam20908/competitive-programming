3c46af38b642cd0764b20e8530601d2e3a954432
Sam Huang <samestimable2016@gmail.com>
2023-07-15 15:45:53 -0700

    Compute smallest divisor in sieve

--- templates/sieve.cpp
+++ templates/sieve.cpp
@@ -1,10 +1,15 @@
 struct sieve {
   vector<int> primes, composite, sd;
   sieve(int n) : composite(n + 1), sd(n + 1) {
+    sd[1] = 1;
     for (int i = 2; i <= n; i++) {
-      if (!composite[i]) primes.push_back(i);
+      if (!composite[i]) {
+        primes.push_back(i);
+        sd[i] = i;
+      }
       for (int j = 0; j < primes.size() && i * primes[j] <= n; j++) {
         composite[i * primes[j]] = true;
+        sd[i * primes[j]] = primes[j];
         if (i % primes[j] == 0) break;
       }
     }
