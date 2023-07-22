090c98c5097a3c02b0d6d7b8d734eebbeef0d6f8
Sam Huang <samestimable2016@gmail.com>
2023-07-21 22:45:50 -0700

    Added binary search template

---
+++ templates/binary_search.cpp
@@ -0,0 +1,18 @@
+template <typename T, typename U> void first_true(T lo, T hi, U f) {
+  ++hi;
+  assert(lo <= hi);
+  while (lo < hi) {
+    T mid = lo + (hi - lo) / 2;
+    f(mid) ? hi = mid : lo = mid + 1;
+  }
+  return lo;
+}
+template <typename T, typename U> T last_true(T lo, T hi, U f) {
+  --lo;
+  assert(lo <= hi);
+  while (lo < hi) {
+    T mid = lo + (hi - lo + 1) / 2;
+    f(mid) ? lo = mid : hi = mid - 1;
+  }
+  return lo;
+}
