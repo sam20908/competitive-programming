291a19cb9b5b12d3a933454e085af59e5faabcfd
Sam Huang <samestimable2016@gmail.com>
2023-07-27 21:35:43 -0700

    Fix segment tree wrong condition

--- templates/segment_tree.cpp
+++ templates/segment_tree.cpp
@@ -1,13 +1,11 @@
 template <typename T> struct segment {
-  static T expand(int l, int r, T v) { return (r - l + 1) * v; }
-  static T combine(T a, T b) { return a + b; }
   int m = 0;
   vector<T> tree;
   segment(int n) : m(n), tree(2 * n) {}
   void update(int i, T v) {
     auto f = [&](auto &self, int tl, int tr, int n) -> T {
       if (tr < i || tl > i || tl > tr) return T();
-      if (tl == tr) return tree[n] += v;
+      if (tl == i && tr == i) return tree[n] += v;
       int mid = tl + (tr - tl) / 2;
       return tree[n] = self(self, tl, mid, n + 1) + self(self, mid + 1, tr, n + 2 * (mid - tl + 1));
     };
