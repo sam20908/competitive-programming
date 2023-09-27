a99edd042f62b24622c1a0d9d2cde4480c62c948
Sam Huang <samestimable2016@gmail.com>
2023-09-27 07:21:49 -0700

    Remove one redundant vector from sieve

--- templates/sieve.cpp
+++ templates/sieve.cpp
@@ -1,17 +1,14 @@
 struct sieve {
-  vector<int> primes, prime, sd;
-  sieve(int n) : prime(n + 1, true), sd(n + 1, 1) {
-    prime[1] = false;
+  vector<int> primes, prime_divisor;
+  sieve(int n) : prime_divisor(n + 1) {
+    iota(prime_divisor.begin(), prime_divisor.end(), 0);
     for (int i = 2; i <= n; i++) {
-      if (prime[i]) {
-        primes.push_back(i);
-        sd[i] = i;
-      }
+      if (prime_divisor[i] == i) primes.push_back(i);
       for (int j = 0; j < (int)primes.size() && i * primes[j] <= n; j++) {
-        prime[i * primes[j]] = false;
-        sd[i * primes[j]] = primes[j];
+        prime_divisor[i * primes[j]] = primes[j];
         if (i % primes[j] == 0) break;
       }
     }
   }
+  bool is_prime(int x) { return x > 1 && prime_divisor[x] == x; }
 };
