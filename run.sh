534e9a1b75f615d4e2a71961ab13b30508a3eef1
Sam Huang <samestimable2016@gmail.com>
2023-10-14 11:39:42 -0700

    Use UBSan with GCC to detect overflows and etc

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-if g++ -D_GLIBCXX_DEBUG -Wall -DLC_LOCAL -std=c++20 main.cpp ; then
+if g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=undefined -DLC_LOCAL -std=c++20 main.cpp ; then
     ./a.out
     printf "\n"
     cat output.txt
