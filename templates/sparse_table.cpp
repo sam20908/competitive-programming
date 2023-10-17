c76bd600493c49a5ecd98aed3b55c0281e7c65ee
Sam Huang <samestimable2016@gmail.com>
2023-10-16 17:18:38 -0700

    Add sparse table

---
+++ templates/sparse_table.cpp
@@ -0,0 +1,28 @@
+template <typename T>
+struct sparse_table
+{
+    vector<vector<T>> dp;
+    int k = 0;
+    sparse_table(vector<T> v) : k(__lg(v.size()))
+    {
+        int n = v.size();
+        dp.resize(k + 1, vector<T>(n));
+        ranges::copy(v, dp[0].begin());
+        for (int i = 1; i <= k; i++)
+            for (int j = 0; j + (1 << i) <= n; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j + (1 << (i - 1))];
+    }
+    T query(int l, int r)
+    {
+        T ans = 0;
+        for (int i = k; i >= 0; i--)
+        {
+            if ((1 << i) <= r - l + 1)
+            {
+                ans += dp[i][l];
+                l += 1 << i;
+            }
+        }
+        return ans;
+    }
+};
