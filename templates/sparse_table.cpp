b6968cd9ce25a894bf6930385ab93b12b1d6e29e
Sam Huang <samestimable2016@gmail.com>
2023-10-16 17:23:55 -0700

    Add O1 query method to sparse table

--- templates/sparse_table.cpp
+++ templates/sparse_table.cpp
@@ -10,19 +10,24 @@ struct sparse_table
         ranges::copy(v, dp[0].begin());
         for (int i = 1; i <= k; i++)
             for (int j = 0; j + (1 << i) <= n; j++)
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j + (1 << (i - 1))];
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
     }
     T query(int l, int r)
     {
-        T ans = 0;
+        T ans = 2e9;
         for (int i = k; i >= 0; i--)
         {
             if ((1 << i) <= r - l + 1)
             {
-                ans += dp[i][l];
+                ans = min(ans, dp[i][l]);
                 l += 1 << i;
             }
         }
         return ans;
+    }
+    T query_O1(int l, int r)
+    { // only use if operation is idempotent!
+        int i = __lg(r - l + 1);
+        return min(st[i][l], st[i][r - (1 << i) + 1]);
     }
 };
