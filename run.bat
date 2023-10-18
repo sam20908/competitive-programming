8ef30271f3592e926a1b7178d97b9ab3ddb650da
Sam Huang <samestimable2016@gmail.com>
2023-10-17 21:53:47 -0700

    Compile the exe with debug info

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=undefined -DLC_LOCAL -std=c++20 main.cpp && goto exec
+g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=undefined -g -DLC_LOCAL -std=c++20 main.cpp && goto exec
 exit /B 1
 :exec
 .\a.out
