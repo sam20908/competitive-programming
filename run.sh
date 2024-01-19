e61874bc2b665aa70263d1c49212f04cf4921a6d
Sam Huang <samestimable2016@gmail.com>
2024-01-19 14:35:09 -0800

    Add address sanitizer for bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-if g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=undefined -g -DLC_LOCAL -std=c++20 main.cpp ; then
+if g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=address,undefined -g -DLC_LOCAL -std=c++20 main.cpp ; then
     ./a.out < input.txt > output.txt
     cat output.txt
 fi
