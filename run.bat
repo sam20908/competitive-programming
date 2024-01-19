19945c0c16a16daac92c51241b2c58adfdc4318f
Sam Huang <samestimable2016@gmail.com>
2024-01-19 14:36:58 -0800

    Disable sanitizers for Windows because they are not supported

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=undefined -g -DLC_LOCAL -std=c++20 main.cpp && goto exec
+g++ -D_GLIBCXX_DEBUG -Wall -g -DLC_LOCAL -std=c++20 main.cpp && goto exec
 exit /B 1
 :exec
 .\a.exe < input.txt > output.txt
