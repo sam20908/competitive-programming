ba6cc57ded34925f7722371a67624bcf9642baa6
sam20908 <samestimable2016@gmail.com>
2024-02-10 17:01:23 -0800

    Rename to spd

--- templates/math/sieve.cpp
+++ templates/math/sieve.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 struct sieve {
-  vector<int> p, pd;
-  sieve(int n) : pd(n + 1) {
-    iota(pd.begin(), pd.end(), 0);
+  vector<int> p, spd;
+  sieve(int n) : spd(n + 1) {
+    iota(spd.begin(), spd.end(), 0);
     for (int i = 2; i <= n; i++) {
-      if (pd[i] == i)
+      if (spd[i] == i)
         p.push_back(i);
       for (int j = 0; j < (int)p.size() && i * p[j] <= n; j++) {
-        pd[i * p[j]] = p[j];
+        spd[i * p[j]] = p[j];
         if (i % p[j] == 0)
           break;
       }
     }
   }
-  bool is_prime(int x) { return x > 1 && pd[x] == x; }
+  bool is_prime(int x) { return x > 1 && spd[x] == x; }
 };
