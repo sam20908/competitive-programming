9032b58713adfa2bd71217989094ef088573445e
Sam Huang <samestimable2016@gmail.com>
2024-03-02 19:13:15 -0800

    Add alias for ordered_set

--- template.cpp
+++ template.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #ifdef LC_LOCAL
 #include "parser.hpp"
@@ -8,6 +11,9 @@ using namespace std;
 #endif
 
 using ll = long long;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // ----- CHANGE FOR PROBLEM -----
 class Solution {
