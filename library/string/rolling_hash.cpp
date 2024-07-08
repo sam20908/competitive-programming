b1f023e5f4c4c2bfbb4aeb9953ccbe707624f3af
Sam Huang <samestimable2016@gmail.com>
2024-07-08 09:07:03 -0700

    Much faster and cleaner rolling hash

--- library/string/rolling_hash.cpp
+++ library/string/rolling_hash.cpp
@@ -1,70 +1,23 @@
-// #define ROLLING_HASH_ENABLE_BASE // need fast exponentiation pow()
 struct rolling_hash {
-  static constexpr int M1 = 1e9 + 7, M2 = 1e9 + 9, P1 = 257, P2 = 261;
-  vector<int> h1, h2;
-  struct precompute {
-    vector<int> p1, p2, i1, i2;
-    void expand(int n) {
-      auto f = [&](vector<int> &pv, vector<int> &iv, int m, int p) {
-        for (long long i = pv.size(), pp = pv.back(); i < n; i++) {
-          pv.push_back(pp = (pp * p) % m);
-#ifdef ROLLING_HASH_ENABLE_BASE
-          iv.push_back(pow(pp, m - 2, m));
-#endif
-        }
-      };
-      f(p1, i1, M1, P1);
-      f(p2, i2, M2, P2);
-    }
-  };
-  static inline auto pre = precompute{{1}, {1}, {1}, {1}};
-  struct proxy_t {
-    int l = 0, r = 0;
-    rolling_hash *ptr = nullptr;
-    int hash1() const {
-      return (ptr->h1[r + 1] - ptr->h1[l] + M1) % M1;
-    }
-    int hash2() const {
-      return (ptr->h2[r + 1] - ptr->h2[l] + M2) % M2;
-    }
-    long long combined() const {
-      return 1LL * hash1() * M2 + hash2();
-    };
-#ifdef ROLLING_HASH_ENABLE_BASE
-    int hash1_base() const {
-      return 1LL * (ptr->h1[r + 1] - ptr->h1[l] + M1) % M1 * pre.i1[l] % M1;
-    }
-    int hash2_base() const {
-      return 1LL * (ptr->h2[r + 1] - ptr->h2[l] + M2) % M2 * pre.i2[l] % M2;
-    }
-    long long combined_base() const {
-      return 1LL * hash1_base() * M2 + hash2_base();
-    };
-#endif
-    friend bool operator==(const proxy_t &a, const proxy_t &b) {
-      long long a1 = a.hash1(), a2 = a.hash2();
-      long long b1 = b.hash1(), b2 = b.hash2();
-      if (b.l > a.l) {
-        a1 = a1 * pre.p1[b.l - a.l] % M1;
-        a2 = a2 * pre.p2[b.l - a.l] % M2;
-      } else if (a.l > b.l) {
-        b1 = b1 * pre.p1[a.l - b.l] % M1;
-        b2 = b2 * pre.p2[a.l - b.l] % M2;
-      }
-      return a1 == b1 && a2 == b2;
-    }
-  };
-  rolling_hash(int n, string &s) : h1(n + 1), h2(n + 1) {
-    pre.expand(n);
-    auto f = [&](vector<int> &h, vector<int> &pv, int m, int p) {
-      for (long long i = 0; i < n; i++) {
-        h[i + 1] = (h[i] + 1LL * s[i] * pv[i]) % m;
-      }
-    };
-    f(h1, pre.p1, M1, P1);
-    f(h2, pre.p2, M2, P2);
+  static constexpr uint64_t M = (1ULL << 61) - 1;
+  static constexpr int P = 257;
+  static constexpr uint64_t mul(uint64_t a, uint64_t b) {
+    uint64_t l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32;
+    uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
+    uint64_t ret = (l & M) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
+    ret = (ret & M) + (ret >> 61);
+    ret = (ret & M) + (ret >> 61);
+    return ret - 1;
   }
-  proxy_t hash(int l, int r) {
-    return {l, r, this};
+  inline static vector<uint64_t> p = vector<uint64_t>{1};
+  vector<uint64_t> h;
+  rolling_hash(int n, string &s) : h(n + 1) {
+    for (uint64_t i = p.size(), pp = p.back(); i <= n; i++)
+      p.push_back(pp = mul(pp, P));
+    for (int i = n - 1; i >= 0; i--)
+      h[i] = (s[i] + mul(h[i + 1], P)) % M;
+  }
+  uint64_t hash(int l, int r) {
+    return (h[l] - mul(h[r + 1], p[r - l + 1]) + M) % M;
   }
 };
