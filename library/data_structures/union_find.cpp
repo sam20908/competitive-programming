81c1293f3b4b494044ffdc6fa5307162a2669a0c
Sam Huang <samestimable2016@gmail.com>
2024-07-13 07:17:33 -0700

    More friendly member names

--- library/data_structures/union_find.cpp
+++ library/data_structures/union_find.cpp
@@ -1,20 +1,20 @@
 struct union_find {
-  vector<int> p, s;
-  union_find(int n) : p(n), s(n, 1) {
-    iota(p.begin(), p.end(), 0);
+  vector<int> par, size;
+  union_find(int n) : par(n), size(n, 1) {
+    iota(par.begin(), par.end(), 0);
   }
   int find(int i) {
-    return p[i] == i ? i : find(p[i]);
+    return par[i] == i ? i : find(par[i]);
   }
   void unite(int i, int j, function<void(int, int)> fn = [](int, int) {}) {
     i = find(i), j = find(j);
     if (i == j)
       return;
-    if (s[i] < s[j])
+    if (size[i] < size[j])
       swap(i, j);
     if (fn)
       fn(i, j);
-    s[i] += s[j];
-    p[j] = i;
+    size[i] += size[j];
+    par[j] = i;
   }
 };
