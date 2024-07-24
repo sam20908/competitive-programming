67e342c29a13a6d78cccebddf7b54974633fc28d
Sam Huang <samestimable2016@gmail.com>
2024-07-24 14:08:29 -0700

    Change combiner -> merger

--- library/data_structures/segment_tree.cpp
+++ library/data_structures/segment_tree.cpp
@@ -2,20 +2,20 @@ template <typename T, typename U, typename V>
 struct segment_tree {
   vector<T> tree;
   U updater;
-  V combiner;
-  segment_tree(int n, T default_value, U updater, V combiner) : tree(2 * n, default_value), updater(updater), combiner(combiner) {}
+  V merger;
+  segment_tree(int n, T default_value, U updater, V merger) : tree(2 * n, default_value), updater(updater), merger(merger) {}
   void update(int i, T v) {
     for (tree[i] = updater(tree[i += tree.size() >> 1], v); i >>= 1;)
-      tree[i] = combiner(tree[i << 1], tree[i << 1 | 1]);
+      tree[i] = merger(tree[i << 1], tree[i << 1 | 1]);
   }
   invoke_result_t<V, T, T> query(int l, int r, invoke_result_t<V, T, T> default_value = {}) { // [l, r)
     auto ansl = default_value, ansr = default_value;
     for (l += tree.size() >> 1, r += tree.size() >> 1; l < r; l >>= 1, r >>= 1) {
       if (l & 1)
-        ansl = combiner(ansl, tree[l++]);
+        ansl = merger(ansl, tree[l++]);
       if (r & 1)
-        ansr = combiner(tree[--r], ansr);
+        ansr = merger(tree[--r], ansr);
     }
-    return combiner(ansl, ansr);
+    return merger(ansl, ansr);
   }
 };
