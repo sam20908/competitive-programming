4873f47aac65e70eca547ed2ae3fa504125e87bf
sam20908 <samestimable2016@gmail.com>
2024-07-26 01:58:08 -0700

    Update union find

--- library/data_structures/union_find.cpp
+++ library/data_structures/union_find.cpp
@@ -1,19 +1,20 @@
+template <invocable<int, int> F>
 struct union_find {
   vector<int> par, size;
-  union_find(int n) : par(n), size(n, 1) {
+  F f;
+  union_find(int n, F f) : par(n), size(n, 1), f(f) {
     iota(par.begin(), par.end(), 0);
   }
   int find(int i) {
     return par[i] == i ? i : find(par[i]);
   }
-  void unite(int i, int j, function<void(int, int)> fn = [](int, int) {}) {
+  void unite(int i, int j) {
     i = find(i), j = find(j);
     if (i == j)
       return;
     if (size[i] < size[j])
       swap(i, j);
-    if (fn)
-      fn(i, j);
+    f(i, j);
     size[i] += size[j];
     par[j] = i;
   }
