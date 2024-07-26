5649fb76615af9581a10d1456aae6b8544d6bdbb
sam20908 <samestimable2016@gmail.com>
2024-07-26 01:18:34 -0700

    Remove n parameter from rolling hash

--- library/string/rolling_hash.cpp
+++ library/string/rolling_hash.cpp
@@ -2,10 +2,10 @@ struct rolling_hash {
   static constexpr uint64_t M = (1ULL << 61) - 1, P = 257;
   inline static vector<uint64_t> p = vector<uint64_t>{1};
   vector<uint64_t> h;
-  rolling_hash(int n, string &s) : h(n + 1) {
-    for (uint64_t i = p.size(), pp = p.back(); i <= n; i++)
+  rolling_hash(const string &s) : h(s.size() + 1) {
+    for (uint64_t i = p.size(), pp = p.back(); i <= s.size(); i++)
       p.push_back(pp = __uint128_t(pp) * P % M);
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = s.size() - 1; i >= 0; i--)
       h[i] = (s[i] + __uint128_t(h[i + 1]) * P % M) % M;
   }
   uint64_t hash(int l, int r) {
