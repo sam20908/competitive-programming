996181a4fe213ebca4f90147eb4b4df696171d6f
sam20908 <samestimable2016@gmail.com>
2024-08-12 12:34:58 -0700

    Ultimate iterative segment tree!

--- library/range_query/segment_tree.cpp
+++ library/range_query/segment_tree.cpp
@@ -1,33 +1,27 @@
-template <typename T, typename A, typename C, typename C2>
+template <typename T, typename A, typename C, typename C2, typename F = identity>
 struct segment_tree {
   vector<T> tree;
   A apply;
   C combine;
   C2 query_combine;
-  segment_tree(int n, T v, A apply, C combine, C2 query_combine) : tree(2 * n, v), apply(apply), combine(combine), query_combine(query_combine) {}
-  void update(int i, T v) {
-    for (tree[i] = apply(tree[i += tree.size() >> 1], v); i >>= 1;)
+  F query_fn;
+  segment_tree(int n, T v, A apply, C combine, C2 query_combine, F query_fn = {}) : tree(2 * n, v), apply(apply), combine(combine), query_combine(query_combine), query_fn(query_fn) {}
+  template <typename... Args>
+  void update(int i, Args &&...args) {
+    int n = tree.size() >> 1;
+    for (apply(tree[i += n], std::forward<Args>(args)...); i >>= 1;)
       tree[i] = combine(tree[i << 1], tree[i << 1 | 1]);
   }
-  template <bool Commutative = true, typename R = T, typename... Args>
+  template <typename R = T, typename... Args>
   R query(int l, int r, R ans = {}, Args &&...args) { // [l, r)
-    if constexpr (Commutative) {
-      for (l += tree.size() >> 1, r += tree.size() >> 1; l < r; l >>= 1, r >>= 1) {
-        if (l & 1)
-          ans = query_combine(ans, tree[l++], std::forward<Args>(args)...);
-        if (r & 1)
-          ans = query_combine(ans, tree[--r], std::forward<Args>(args)...);
-      }
-      return ans;
-    } else {
-      auto ansl = ans, ansr = ans;
-      for (l += tree.size() >> 1, r += tree.size() >> 1; l < r; l >>= 1, r >>= 1) {
-        if (l & 1)
-          ansl = query_combine(ansl, tree[l++], std::forward<Args>(args)...);
-        if (r & 1)
-          ansr = query_combine(tree[--r], ansr, std::forward<Args>(args)...);
-      }
-      return query_combine(ansl, ansr, std::forward<Args>(args)...);
+    int n = tree.size() >> 1;
+    auto ansl = ans, ansr = ans;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l & 1)
+        ansl = query_combine(ansl, query_fn(tree[l++], std::forward<Args>(args)...));
+      if (r & 1)
+        ansr = query_combine(query_fn(tree[--r], std::forward<Args>(args)...), ansr);
     }
+    return query_combine(ansl, ansr);
   }
 };
