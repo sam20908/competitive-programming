222b6a409968f4a3ba958210923fda78f73309cd
sam20908 <samestimable2016@gmail.com>
2024-12-29 07:21:40 -0800

    Reworked modular related templates to be more flexible

--- library/math/factorials.cpp
+++ library/math/factorials.cpp
@@ -1,17 +1,17 @@
-template <int M>
+template <int M, typename T = int>
 struct factorials {
-  vector<long long> fact, ifact, inv;
+  vector<T> fact, ifact, inv;
   factorials(int n) : fact(n + 1, 1), ifact(n + 1, 1), inv(n + 1, 1) {
     for (int i = 2; i <= n; i++) {
-      fact[i] = fact[i - 1] * i % M;
+      fact[i] = 1LL * fact[i - 1] * i % M;
       inv[i] = 1LL * (M - M / i) * inv[M % i] % M;
-      ifact[i] = ifact[i - 1] * inv[i] % M;
+      ifact[i] = 1LL * ifact[i - 1] * inv[i] % M;
     }
   }
-  long long ncr(int n, int k) {
-    return fact[n] * ifact[k] % M * ifact[n - k] % M;
+  T ncr(int n, int k) const {
+    return 1LL * fact[n] * ifact[k] % M * ifact[n - k] % M;
   }
-  long long npr(int n, int k) {
-    return fact[n] * ifact[n - k] % M;
+  T npr(int n, int k) const {
+    return 1LL * fact[n] * ifact[n - k] % M;
   }
 };
---
+++ library/math/modnum.cpp
@@ -0,0 +1,38 @@
+template <int M, typename T = int>
+struct modnum {
+  T v{};
+  constexpr modnum() = default;
+  constexpr modnum(long long vx) : v((vx % M + M) % M) {}
+  constexpr modnum &operator=(long long vx) {
+    v = (vx % M + M) % M;
+    return *this;
+  }
+  constexpr modnum<M, T> pow(long long p) const {
+    long long a = v, res = 1;
+    for (; p; p >>= 1, a = a * a % M)
+      if (p & 1)
+        res = res * a % M;
+    return res;
+  }
+  constexpr modnum<M, T> inv() const {
+    return pow(M - 2);
+  }
+  constexpr explicit operator T() const {
+    return v;
+  }
+  constexpr modnum<M, T> operator%(long long mod) const {
+    return v % mod;
+  }
+#define OP(op)                                                                                                                                                                                                                                                                                                                                                                                                 \
+  constexpr friend modnum operator op(const modnum &a, const modnum &b) {                                                                                                                                                                                                                                                                                                                                      \
+    return ((1LL * a.v op b.v) % M + M) % M;                                                                                                                                                                                                                                                                                                                                                                   \
+  }                                                                                                                                                                                                                                                                                                                                                                                                            \
+  constexpr modnum &operator op##=(const modnum & other) {                                                                                                                                                                                                                                                                                                                                                     \
+    v = ((1LL * v op other.v) % M + M) % M;                                                                                                                                                                                                                                                                                                                                                                    \
+    return *this;                                                                                                                                                                                                                                                                                                                                                                                              \
+  }
+  OP(+)
+  OP(-)
+  OP(*)
+#undef OP
+};
