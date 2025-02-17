951d4cea9a554884ce1afd74fecf1daa9dae4026
sam20908 <samestimable2016@gmail.com>
2025-02-17 14:23:30 -0800

    Split mo_array and hilbert

--- library/range_query/mo_array.cpp
+++ library/range_query/mo_array.cpp
@@ -8,28 +8,14 @@ struct mo_array {
   void add_query(int l, int r) {
     q.push_back({l, r});
   };
-  auto solve() {
+  template <typename H>
+  auto solve(H &&heuristic) {
     int n = q.size();
-    auto hilbert = q | views::transform([](auto p) {
-                     int x = p.first, y = p.second;
-                     const uint64_t l = __lg(max(x, y) * 2 + 1) | 1;
-                     const uint64_t mx = (1ull << l) - 1;
-                     uint64_t ans = 0;
-                     for (uint64_t s = 1ull << (l - 1); s; s >>= 1) {
-                       bool rx = x & s, ry = y & s;
-                       ans = (ans << 2) | (rx ? ry ? 2 : 1 : ry ? 3 : 0);
-                       if (!rx) {
-                         if (ry)
-                           x ^= mx, y ^= mx;
-                         swap(x, y);
-                       }
-                     }
-                     return ans;
-                   }) |
-        ranges::to<vector>();
-    auto ord = views::iota(0, n) | ranges::to<vector>();
-    ranges::sort(ord, {}, [&](int i) {
-      return hilbert[i];
+    auto h = heuristic(q);
+    vector<int> ord(n);
+    iota(ord.begin(), ord.end(), 0);
+    ranges::sort(ord, [&](int i, int j) {
+      return h[i] < h[j];
     });
     int cl = 0, cr = -1;
     vector<long long> ans(n);
---
+++ library/sortings/hilbert.cpp
@@ -0,0 +1,20 @@
+vector<uint64_t> hilbert(const vector<pair<int, int>> &q) {
+  int n = q.size();
+  vector<uint64_t> ans(n);
+  for (int i = 0; i < n; i++) {
+    auto [x, y] = q[i];
+    const uint64_t l = __lg(max(x, y) * 2 + 1) | 1;
+    const uint64_t mx = (1ull << l) - 1;
+    uint64_t ord = 0;
+    for (uint64_t s = 1ull << (l - 1); s; s >>= 1) {
+      bool rx = x & s, ry = y & s;
+      ord = (ord << 2) | (rx ? ry ? 2 : 1 : ry ? 3 : 0);
+      if (!rx) {
+        if (ry)
+          x ^= mx, y ^= mx;
+        swap(x, y);
+      }
+    }
+    ans[i] = ord;
+  }
+}
