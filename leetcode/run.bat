1664268518dbc4ccd400f830de527fc6ebb6fdc3
sam20908 <samestimable2016@gmail.com>
2025-04-25 13:00:42 -0700

    Remove redundant build scripts

--- leetcode/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-REM AddressSanitizer and UndefinedSanitizer are not supported on Windows' GCC builds
-g++ -D_GLIBCXX_DEBUG -Wall -g -DLC_LOCAL -std=c++20 %1 && .\a.exe
--- leetcode/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if g++ -D_GLIBCXX_DEBUG -Wall -fsanitize=address,undefined -g -DLC_LOCAL -std=c++20 $1 ; then
-	./a.out
-fi
