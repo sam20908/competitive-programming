63b8dd7be9d7273c1bec497c6c9eb21ec7a8997c
Sam Huang <samestimable2016@gmail.com>
2025-08-08 13:41:19 -0700

    Add KMP

---
+++ library/string/kmp.cpp
@@ -0,0 +1,12 @@
+vector<int> kmp(const string &s) {
+  int n = s.size();
+  vector<int> pi(n);
+  for (int i = 1; i < n; i++) {
+    int j = i;
+    for (; j > 0 && s[j] != s[i]; j = pi[j - 1]) {}
+    if (s[j] == s[i])
+      j++;
+    pi[i] = j;
+  }
+  return pi;
+}
