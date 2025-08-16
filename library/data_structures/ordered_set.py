0c724fbd2b29547be89187437717aedf08316891
Sam Huang <samestimable2016@gmail.com>
2025-08-16 15:57:33 -0700

    Use Perfect_Leaves subroutine and update sizes as we rotate

--- library/data_structures/ordered_set.py
+++ library/data_structures/ordered_set.py
@@ -13,7 +13,6 @@ class OrderedSet:
     ALPHA = 0.85  # 0.5 (faster lookups) < ALPHA < 1 (faster modifications)
 
     def __init__(self, code=None, multiset=False):
-        # start with index 1 being the free slot (we don't need to keep extra spot for value)
         self.value = array(code, [0]) if code else [None]
         self.left = array("i", [0, 0])
         self.right = array("i", [0, 0])
@@ -26,68 +25,63 @@ class OrderedSet:
     def __len__(self):
         return self.nodes
 
-    def _compress(self, root, size):
-        for _ in range(size):
-            child = self.right[root]
-            self.right[root] = self.right[child]
-            root = self.right[root]
-            self.right[child] = self.left[root]
-            self.left[root] = child
-
     def _rebalance(self, root):
         size = self.size[root]
-        dummy = self.free  # use the free spot for dummy node
+        dummy = self.free
         prev_free = self.left[dummy]
         self.left[dummy] = 0
         self.right[dummy] = root
         tail = dummy
-        rest = self.right[tail]
-        while rest:
-            if self.left[rest]:
-                tmp = self.left[rest]
-                self.left[rest] = self.right[tmp]
-                self.right[tmp] = rest
-                rest = tmp
-                self.right[tail] = tmp
+        cur = root
+        while cur:
+            left = self.left[cur]
+            if left:
+                left_right = self.right[left]
+                self.size[cur] += self.size[left_right] - self.size[left]
+                self.size[left] += self.size[cur] - self.size[left_right]
+                self.left[cur] = left_right
+                self.right[left] = cur
+                cur = left
+                self.right[tail] = left
             else:
-                tail = rest
-                rest = self.right[rest]
-        leaves = size + 1 - (1 << (size + 1).bit_length() - 1)
-        self._compress(dummy, leaves)
+                tail = cur
+                cur = self.right[cur]
+        leaf_positions = 1 << (size + 1).bit_length() - 1
+        leaves = size + 1 - leaf_positions
+        if leaves > 0:
+            hole_count = leaf_positions - leaves
+            hole_index = 1
+            next_hole = leaf_positions // hole_count
+            cur = dummy
+            for i in range(1, leaf_positions):
+                if i == next_hole:
+                    cur = self.right[cur]
+                    hole_index += 1
+                    next_hole = (hole_index * leaf_positions) // hole_count
+                else:
+                    leaf = self.right[cur]
+                    self.right[cur] = self.right[leaf]
+                    cur = self.right[cur]
+                    self.size[leaf] -= self.size[cur]
+                    self.size[cur] += self.size[leaf]
+                    self.left[cur] = leaf
+                    self.right[leaf] = 0
         size -= leaves
         while size > 1:
             size >>= 1
-            self._compress(dummy, size)
+            cur = dummy
+            for _ in range(size):
+                root = self.right[cur]
+                self.right[cur] = self.right[root]
+                cur = self.right[cur]
+                root_left = self.left[cur]
+                self.size[root] += self.size[root_left] - self.size[cur]
+                self.size[cur] += self.size[root] - self.size[root_left]
+                self.right[root] = self.left[cur]
+                self.left[cur] = root
+
         self.left[dummy] = self.right[dummy]
         self.right[dummy] = 0
-        root = dummy
-        while root:
-            self.size[root] = 1
-            if self.left[root]:
-                cur = self.left[root]
-                while self.right[cur] and self.right[cur] != root:
-                    cur = self.right[cur]
-                if self.right[cur]:
-                    self.right[cur] = 0
-                    parent = root
-                    cur = self.left[root]
-                    while cur:
-                        nxt = self.right[cur]
-                        self.right[cur] = parent
-                        parent = cur
-                        cur = nxt
-                    while parent != root:
-                        nxt = self.right[parent]
-                        self.size[nxt] += self.size[parent]
-                        self.right[parent] = cur
-                        cur = parent
-                        parent = nxt
-                    root = self.right[root]
-                else:
-                    self.right[cur] = root
-                    root = self.left[root]
-            else:
-                root = self.right[root]
         root = self.left[dummy]
         self.left[dummy] = prev_free
         return root
