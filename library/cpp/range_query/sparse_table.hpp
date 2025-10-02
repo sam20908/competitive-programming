284eb4db7ae7e45d45f54cbd67e82aa949d81571
Sam Huang <samestimable2016@gmail.com>
2025-10-02 15:21:33 -0700

    `sparse_table`: Update to fit its practical purpose of constant time queries.

--- library/cpp/range_query/sparse_table.hpp
+++ library/cpp/range_query/sparse_table.hpp
@@ -2,46 +2,27 @@
 using namespace std;
 
 /**
- * @brief Answers subarray queries in `O(log N)` (or `O(1)` if the operation is
- * idempotent) with `O(N log N)` preprocessing.
+ * @brief Answers subarray queries in `O(1)` with `O(N log N)` preprocessing.
  *
- * Sparse Tables work well when the input data stays constant. The main benefit
- * is the constant factor is very low.
+ * Sparse Tables are immutable, but they can answer range queries very fast if
+ * the answer remains correct when merging answers of two (possibly) overlapping
+ * ranges. Some example operations are `gcd` and `min`.
  *
  * @tparam T Value type.
- * @tparam F Binary functor to merge two values.
+ * @tparam Combine Binary functor to merge two values.
  */
-template <typename T, typename F> class sparse_table {
+template <typename T, typename Combine> class sparse_table {
   vector<vector<T>> dp;
-  F f;
+  Combine combine;
 
 public:
-  /**
-   * @brief Constructs a Sparse Table for a given array.
-   */
-  sparse_table(vector<T> &v, const F &f) : f(f) {
+  sparse_table(const vector<T> &v, const Combine &combine) : combine(combine) {
     int n = v.size();
     dp.resize(__lg(n) + 1, vector<T>(n));
     dp[0] = v;
     for (int i = 1; i <= __lg(v.size()); i++)
       for (int j = 0; j + (1 << i) <= v.size(); j++)
-        dp[i][j] = f(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
-  }
-
-  /**
-   * @brief Queries the range `[l, r]` in `O(log N)`.
-   *
-   * @param l Left bound.
-   * @param r Right bound.
-   */
-  optional<T> query(int l, int r) const {
-    optional<T> ans;
-    for (int i = dp.size(); i >= 0; i--)
-      if ((1 << i) <= r - l + 1) {
-        ans = ans ? f(*ans, dp[i][l]) : dp[i][l];
-        l += 1 << i;
-      }
-    return ans;
+        dp[i][j] = combine(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
   }
 
   /**
@@ -50,8 +31,8 @@ public:
    * @param l Left bound.
    * @param r Right bound.
    */
-  optional<T> query_idempotent(int l, int r) const {
+  T query(int l, int r) const {
     int i = __lg(r - l + 1);
-    return f(dp[i][l], dp[i][r - (1 << i) + 1]);
+    return combine(dp[i][l], dp[i][r - (1 << i) + 1]);
   }
 };
