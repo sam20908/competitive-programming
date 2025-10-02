dd2a0152a13863666d318ddd704012a1fb3f12d9
Sam Huang <samestimable2016@gmail.com>
2025-10-02 01:00:14 -0700

    Add `segment_tree` test

---
+++ test/checker/segment_tree.py
@@ -0,0 +1,29 @@
+from test.lib import cli, runner
+from random import randint
+
+
+def main():
+    args = cli.args.parse_args()
+    iter = args.iterations
+    for _ in range(iter):
+        stdin = []
+
+        n = randint(1, 2 * 10**5)
+        q = randint(1, 2 * 10**5)
+        stdin.append(f"{n} {q}")
+
+        x = [randint(-(10**9), 10**9) for _ in range(n)]
+        stdin.append(" ".join(map(str, x)))
+
+        for _ in range(q):
+            type = randint(1, 2)
+            a = randint(1, n)
+            b = randint(a, n)
+            stdin.append(f"{type} {a} {b}")
+
+        proc_ans, sol_ans = runner.run(args.program, args.solution, "\n".join(stdin))
+        assert proc_ans == sol_ans
+
+
+if __name__ == "__main__":
+    main()
