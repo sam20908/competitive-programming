f0a6133900ab933c6d62d4355bd001b5cf5ef7a3
Sam Huang <samestimable2016@gmail.com>
2025-10-03 02:49:29 -0700

    Update random.py

--- test/lib/random.py
+++ test/lib/random.py
@@ -1,7 +1,7 @@
-from random import randint
+from random import randint, sample
 
 
-def rand_tree_edges(n: int) -> list[list[int]]:
+def rand_tree_edges(n: int) -> list[tuple[int, int]]:
     """
     Generates a random tree in linear time using random Prufer Code.
 
@@ -18,7 +18,7 @@ def rand_tree_edges(n: int) -> list[list[int]]:
     leaf = ptr
     ed = []
     for v in code:
-        ed.append([leaf + 1, v + 1])
+        ed.append((leaf + 1, v + 1))
         deg[v] -= 1
         if deg[v] == 1 and v < ptr:
             leaf = v
@@ -27,7 +27,7 @@ def rand_tree_edges(n: int) -> list[list[int]]:
             while deg[ptr] != 1:
                 ptr += 1
             leaf = ptr
-    ed.append([ptr + 1, n])
+    ed.append((ptr + 1, n))
     return ed
 
 
@@ -47,3 +47,33 @@ def rand_tree_parent_list(n: int, root: int = 1) -> list[int]:
 
     dfs(root, -1)
     return ans[1:]
+
+
+def rand_connected_graph_edges(n: int, m: int):
+    assert n - 1 <= m <= n * (n - 1) // 2
+    ed = rand_tree_edges(n)
+    ex = set()
+    deg = [0] * (n + 1)
+    for a, b in ed:
+        ex.add((a, b))
+        ex.add((b, a))
+        deg[a] += 1
+        deg[b] += 1
+
+    choices = []
+    for i in range(1, n + 1):
+        if deg[i] < n - 1:
+            choices.append(i)
+
+    for _ in range(m - len(ed)):
+        assert len(choices) >= 2
+        a, b = sample(choices, 2)
+        ed.append((a, b))
+        deg[a] += 1
+        deg[b] += 1
+        if deg[a] == n - 1:
+            deg.remove(a)
+        if deg[b] == n - 1:
+            deg.remove(b)
+
+    return ed
